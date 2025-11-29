a29e84fc5c39101f101698fec09c13f725d6b384
michaelquesado <esquesado@gmail.com>
2025-11-29 16:44:17 -0300

    feature: loading env

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import http from 'node:http'
 
+import dotevn from 'dotenv'
+
+dotevn.config()
+
 const server = http.createServer((_, res) => {
+    console.log({APP_COOL_SECRET: process.env.APP_COOL_SECRET })
     return res.end("Hello World")
 })
 
